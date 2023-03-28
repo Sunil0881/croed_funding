864a965b8c9dec9a2b7a1bd049169f594f0c573f
Sunil088 <sunil12162003@gmail.com>
2023-03-28 17:27:16 +0530

    smart contract

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -1,6 +1,51 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
-contract MyContract {
-    constructor() {}
+contract CrowdFunding {
+    struct Campaign {
+        address owner;
+        string title;
+        string description;
+        uint amountCollected;
+        uint target;
+        uint deadline;
+        string image;
+        address[] donators;
+        uint[] donations;
+    }
+
+    mapping(uint => Campaign) public campaigns;
+
+    uint public numberOfCampaigns = 0;
+
+    function createCampaign(
+        address _owner,
+        string memory _title,
+        string memory _description,
+        uint _amountCollected,
+        uint _target,
+        uint _deadline,
+        string memory _image
+    ) public returns (uint) {
+        Campaign storage campaign = campaigns[numberOfCampaigns];
+
+        require(
+            campaign.deadline < block.timestamp,
+            "deadline should be in the future"
+        );
+
+        campaign.owner = _owner;
+        campaign.title = _title;
+        campaign.description = _description;
+        campaign.amountCollected = _amountCollected;
+        campaign.target = _target;
+        campaign.deadline = _deadline;
+        campaign.image = _image;
+
+        numberOfCampaigns++;
+
+        return (numberOfCampaigns - 1);
+    }
+
+  
 }
