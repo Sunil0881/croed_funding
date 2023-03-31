39b3d2e1e663357adcb7eea0978c9d53c29e4721
Sunil088 <sunil12162003@gmail.com>
2023-03-31 07:25:57 +0530

    completed contract

--- contracts/Contract.sol
+++ contracts/Contract.sol
@@ -47,5 +47,34 @@ contract CrowdFunding {
         return (numberOfCampaigns - 1);
     }
 
-  
+    function donateToCampaign(uint _id) public payable {
+        uint amount = msg.value;
+
+        Campaign storage campaign = campaigns[_id];
+
+        campaign.donators.push(msg.sender);
+        campaign.donations.push(amount);
+
+        (bool sent, ) = payable(campaign.owner).call{value: amount}("");
+
+        if (sent) {
+            campaign.amountCollected = campaign.amountCollected + amount;
+        }
+    }
+
+    function getDonators(
+        uint _id
+    ) public view returns (address[] memory, uint[] memory) {
+        return (campaigns[_id].donators, campaigns[_id].donations);
+    }
+
+    function getCampaigns() public view returns (Campaign[] memory) {
+        Campaign[] memory allCampaigns = new Campaign[](numberOfCampaigns);
+
+        for (uint i = 0; i < numberOfCampaigns; i++) {
+            Campaign storage item = campaigns[i];
+            allCampaigns[i] = item;
+        }
+        return allCampaigns;
+    }
 }
